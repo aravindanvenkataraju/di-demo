fa0ab7026875a03b5efd0b6d7645499be770644e
Aravindan Venkataraju <aravindan.venkataraju@gmail.com>
2019-02-25 01:14:37 +1100

    all-different-di-options

--- src/main/java/com/aravindan/springframework/didemo/DiDemoApplication.java
+++ src/main/java/com/aravindan/springframework/didemo/DiDemoApplication.java
@@ -2,12 +2,23 @@ package com.aravindan.springframework.didemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.aravindan.springframework.didemo.controllers.ConstructorInjectedController;
+import com.aravindan.springframework.didemo.controllers.MyController;
+import com.aravindan.springframework.didemo.controllers.PropertyInjectedController;
+import com.aravindan.springframework.didemo.controllers.SetterInjectedController;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiDemoApplication.class, args);
+		ApplicationContext ctx =  SpringApplication.run(DiDemoApplication.class, args);
+		MyController controller = ctx.getBean(MyController.class, "myController");
+		controller.hello();
+		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
+		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
 	}
 
 }
