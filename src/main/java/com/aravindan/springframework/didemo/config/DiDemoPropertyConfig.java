2f7703c37c28f0031132df75083adf0087af9ce9
Aravindan Venkataraju <aravindan.venkataraju@gmail.com>
2019-03-03 17:14:49 +1100

    getting environnment variable values

--- src/main/java/com/aravindan/springframework/didemo/config/DiDemoPropertyConfig.java
+++ src/main/java/com/aravindan/springframework/didemo/config/DiDemoPropertyConfig.java
@@ -1,15 +1,21 @@
 package com.aravindan.springframework.didemo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 import com.aravindan.springframework.didemo.DiDemoDataSource;
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class DiDemoPropertyConfig {
+	
+	@Autowired
+	Environment env;
+	
 	@Value("${com.aravindan.springframework.username}")
 	String user;
 	
@@ -27,7 +33,7 @@ public class DiDemoPropertyConfig {
 	@Bean
 	DiDemoDataSource diDemoDataSource() {
 		DiDemoDataSource diDemoDataSource = new DiDemoDataSource();
-		diDemoDataSource.setUser(user);
+		diDemoDataSource.setUser(env.getProperty("DIDEMO_USERNAME"));
 		diDemoDataSource.setPassword(password);
 		diDemoDataSource.setUrl(url);
 		return diDemoDataSource;
