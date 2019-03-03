41a20c2b62fcbfed122737758bf53da7fd2e8c44
Aravindan Venkataraju <aravindan.venkataraju@gmail.com>
2019-03-03 17:22:37 +1100

    using application.properties for custom properties

--- src/main/java/com/aravindan/springframework/didemo/config/DiDemoPropertyConfig.java
+++ src/main/java/com/aravindan/springframework/didemo/config/DiDemoPropertyConfig.java
@@ -5,12 +5,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 import com.aravindan.springframework.didemo.DiDemoDataSource;
+import com.aravindan.springframework.didemo.DiDemoJmsBroker;
 @Configuration
-@PropertySource("classpath:datasource.properties")
+//
+//@PropertySources({
+//	@PropertySource("classpath:datasource.properties"),
+//	@PropertySource("classpath:jms.properties")
+//})
 public class DiDemoPropertyConfig {
 	
 	@Autowired
@@ -25,10 +31,19 @@ public class DiDemoPropertyConfig {
 	@Value("${com.aravindan.springframework.dburl}")
 	String url;
 	
-	public static PropertySourcesPlaceholderConfigurer properties() {
-		PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-		return propertySourcesPlaceholderConfigurer;
-	}
+	@Value("${didemo.jms.username}")
+	String jmsUser;
+	
+	@Value("${didemo.jms.password}")
+	String jmsPassword;
+	
+	@Value("${didemo.jms.jmsurl}")
+	String jmsUrl;
+	
+//	public static PropertySourcesPlaceholderConfigurer properties() {
+//		PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+//		return propertySourcesPlaceholderConfigurer;
+//	}
 	
 	@Bean
 	DiDemoDataSource diDemoDataSource() {
@@ -37,6 +52,16 @@ public class DiDemoPropertyConfig {
 		diDemoDataSource.setPassword(password);
 		diDemoDataSource.setUrl(url);
 		return diDemoDataSource;
+		
+	}
+	
+	@Bean
+	DiDemoJmsBroker diDemoJmsBroker() {
+		DiDemoJmsBroker diDemoJmsBroker = new DiDemoJmsBroker();
+		diDemoJmsBroker.setUser(jmsUser);
+		diDemoJmsBroker.setPassword(jmsPassword);
+		diDemoJmsBroker.setUrl(jmsUrl);
+		return diDemoJmsBroker;
 		
 	}
 
